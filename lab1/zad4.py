c9e5945deae39a73c269c026f45e57ca1fbe1a0a
Denys Kuzhyk <kuzhyk2002@gmail.com>
2022-03-11 23:26:25 +0100

    zad4

---
+++ lab1/zad4.py
@@ -0,0 +1,12 @@
+import math as m
+import numpy
+#zadanie4
+
+e = 10
+i = e**10 #10000000000
+
+eq = numpy.log(5+(m.sin(8))**2)
+result = eq**(1/6)
+
+round_floor = m.floor(3.55)
+round_ceil = m.ceil(4.80)
